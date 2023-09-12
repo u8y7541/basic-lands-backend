f8a85683a0fe9227b44eefee51e632537b323b8f
u8y7541 <surya.subbarao1@gmail.com>
2023-09-11 18:18:51 -0700

    Most of game logic

--- Game.js
+++ Game.js
@@ -44,11 +44,16 @@ class Game {
     setupSockets() {
         console.log("Setup sockets");
         [0,1].forEach((i) => {
-            this.sockets[i].on("play card", (args) => this.playCard(i, args));
-            this.sockets[i].on("end turn", () => this.endTurn(i));
-            console.log("Sending board state to player "+i);
-            this.sockets[i].emit("game started", this.gameID);
-            this.sockets[i].emit("board state", this.getBoardState(i));
+            let sock = this.sockets[i];
+            sock.on("play card", (args) => this.playCard(i, args));
+            sock.on("mountain select", (args) => this.mountainSelect(i, args));
+            sock.on("forest select", (args) => this.forestSelect(i, args));
+            sock.on("island select", (args) => this.islandSelect(i, args));
+            sock.on("swamp select", (args) => this.swampSelect(i, args));
+            sock.on("end turn", () => this.endTurn(i));
+
+            sock.emit("game started", this.gameID);
+            sock.emit("board state", this.getBoardState(i));
         });
     }
 
@@ -61,31 +66,124 @@ class Game {
             "hand": myFullInfo.hand,
             "board": myFullInfo.board
         };
+        if (this.state === ISLAND_SELECT && player === this.curPlayer) {
+            let islandLength = Math.min(myFullInfo.deck.length,4);
+            mySide["top4"] = myFullInfo.deck.slice(myFullInfo.deck.length-islandLength);
+        }
         let otherSide = {
             "deck": otherFullInfo.deck.length,
             "discard": otherFullInfo.discard,
-            "hand": { "visible" : otherFullInfo.hand.visible.length,
+            "hand": { "visible" : otherFullInfo.hand.visible,
                       "hidden" : otherFullInfo.hand.hidden.length },
             "board": otherFullInfo.board
         }
 
-        return {"myTurn": player == this.curPlayer, "state": [mySide, otherSide]};
+        return {"myTurn": player === this.curPlayer, "state": this.state, "cards": [mySide, otherSide]};
+    }
+
+    updatePlayers() {
+        [0,1].forEach((i) => this.sockets[i].emit("board state", this.getBoardState(i)));
     }
 
     playCard(player, args) {
-        let hand = this.players[player].hand;
-        if (!args.index || !args.type || args.index >= hand.length || hand[args.index] != args.type) {
-            this.sockets[player].emit("play card", false);
+        console.log("Player "+player+" playing card: "+JSON.stringify(args,null,4));
+        let p = this.players[player];
+        let q = this.players[1-player];
+        let hand = (args.visible ? p.hand.visible : p.hand.hidden);
+        if (this.curPlayer !== player || !("index" in args) || !("type" in args) ||
+            !("visible" in args) || (typeof args.visible) !== "boolean" ||
+            (typeof args.index) !== "number" || args.index < 0 ||
+            args.index >= hand.length || !(cardTypes.includes(args.type)) ||
+            hand[args.index] !== args.type) {
+            console.log("Invalid");
             return;
         }
 
-        // TODO
+        hand.splice(args.index, 1);
+        // TODO: island negation
+        p.board[args.type]++;
+        switch (args.type) {
+            case PLAINS:
+                p.hand.hidden.push(p.deck.pop());
+                this.endTurn(player);
+                break;
+            case MOUNTAIN:
+                this.state = MOUNTAIN_SELECT;
+                break;
+            case FOREST:
+                this.state = FOREST_SELECT;
+                break;
+            case ISLAND:
+                this.state = ISLAND_SELECT;
+                break;
+            case SWAMP:
+                this.state = SWAMP_SELECT;
+                q.hand.visible = q.hand.visible.concat(q.hand.hidden);
+                q.hand.hidden = [];
+                break;
+        }
+        this.updatePlayers();
+    }
+
+    // TODO: better type checking for all of these
+    mountainSelect(player, args) {
+        let [p, q] = [this.players[player], this.players[1-player]];
+        if (this.curPlayer !== player || this.state != MOUNTAIN_SELECT ||
+            !(cardTypes.includes(args.type)) || q.board[args.type] < 1) {
+            console.log("Invalid mountain");
+            return;
+        }
+        q.board[args.type]--;
+        q.discard.push(args.type);
+        this.endTurn(player);
+    }
+
+    forestSelect(player, args) {
+        let [p, q] = [this.players[player], this.players[1-player]];
+        if (this.curPlayer !== player || this.state != FOREST_SELECT ||
+            !cardTypes.includes(args.type) || !p.discard.includes(args.type)) {
+            console.log("Invalid forest");
+            return;
+        }
+        let i = p.discard.findIndex(args.type);
+        p.discard.splice(i, 1);
+        p.hand.visible.push(args.type);
+        this.endTurn(player);
+    }
+
+    islandSelect(player, args) {
+        let [p, q] = [this.players[player], this.players[1-player]];
+        let islandLength = Math.min(p.deck.length,4);
+        if (this.curPlayer !== player || this.state != ISLAND_SELECT ||
+            args.some((x) => (x<0 || x>=islandLength)) ||
+            (new Set(args)).size != args.length) {
+            console.log("Invalid island");
+            return;
+        }
+        let newFour = args.map((x) => p.deck[p.deck.length-1-x]);
+        newFour.reverse();
+        p.deck.splice(p.deck.length-islandLength, islandLength, ...newFour);
+        this.endTurn(player);
+    }
+
+    swampSelect(player, args) {
+        let [p, q] = [this.players[player], this.players[1-player]];
+        if (this.curPlayer !== player || this.state != SWAMP_SELECT ||
+            args.index < 0 || args.index >= q.hand.visible.length ||
+            !cardTypes.includes(args.type) || q.hand.visible[args.index] !== args.type) {
+            console.log("Invalid swamp");
+            return;
+        }
+        q.hand.visible.splice(args.index, 1);
+        q.discard.push(args.type);
+        this.endTurn(player);
     }
 
     endTurn(player) {
         if (player !== this.curPlayer) return;
         this.curPlayer = 1-this.curPlayer;
-        [0,1].forEach((i) => this.sockets[i].emit("new turn", this.getBoardState(i)));
+        this.state = TURN_START;
+        this.updatePlayers();
     }
 }
 
